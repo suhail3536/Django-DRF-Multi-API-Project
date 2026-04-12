2b0838cab610ebd32215b8bbdf43a864355cbdcf
Suhail <shab35360@gmail.com>
2026-04-12 13:38:40 +0530

    fixed signals config

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'api',
     'employees',
     'worker',
-    'employee.apps.EmployeeConfig', 
+    'employees.apps.EmployeeConfig', 
 ]
 
 MIDDLEWARE = [
--- employees/apps.py
+++ employees/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
 
-
 class EmployeesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'employees'
 
     def ready(self):
-        import employee.signals
+        import employees.signals
